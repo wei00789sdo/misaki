abb483f3721ecfbe6bdc0618c8425a1307ae9462
Misaki <34979022+wei00789sdo@users.noreply.github.com>
2018-07-06 13:54:17 +0800

    Add files via upload

---
+++ My/src/main/java/com/typemoon/misaki/MisakiApplication.java
@@ -0,0 +1,12 @@
+package com.typemoon.misaki;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MisakiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MisakiApplication.class, args);
+    }
+}
---
+++ My/src/test/java/com/typemoon/misaki/MisakiApplicationTests.java
@@ -0,0 +1,16 @@
+package com.typemoon.misaki;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MisakiApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
